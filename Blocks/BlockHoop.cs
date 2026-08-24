72abf818225a6654301e96b6761914405147eb0d
DoctorLaplace <silvervalifor@gmail.com>
2026-08-24 09:29:01 -0700

    Make basketball hoop thin large white ring spanning full block bounds

--- Blocks/BlockHoop.cs
+++ Blocks/BlockHoop.cs
@@ -12,24 +12,24 @@ namespace BasketballAllstars.Blocks
         public Vec3d GetRimCenter(BlockPos pos)
         {
             // Center of the hoop ring at the top of the block
-            return new Vec3d(pos.X + 0.5, pos.Y + 0.90, pos.Z + 0.5);
+            return new Vec3d(pos.X + 0.5, pos.Y + 0.95, pos.Z + 0.5);
         }
 
         public override Cuboidf[] GetCollisionBoxes(IBlockAccessor blockAccessor, BlockPos pos)
         {
-            // 4 thin border bars of the top ring with open center
+            // 4 thin border bars of the top ring with wide open center
             return new Cuboidf[]
             {
-                new Cuboidf(0.125f, 0.875f, 0.125f, 0.875f, 1.0f, 0.25f),  // North bar
-                new Cuboidf(0.125f, 0.875f, 0.75f,  0.875f, 1.0f, 0.875f), // South bar
-                new Cuboidf(0.125f, 0.875f, 0.25f,  0.25f,  1.0f, 0.75f),  // West bar
-                new Cuboidf(0.75f,  0.875f, 0.25f,  0.875f, 1.0f, 0.75f)   // East bar
+                new Cuboidf(0.0f,    0.9375f, 0.0f,    1.0f, 1.0f, 0.0625f), // North bar
+                new Cuboidf(0.0f,    0.9375f, 0.9375f, 1.0f, 1.0f, 1.0f),    // South bar
+                new Cuboidf(0.0f,    0.9375f, 0.0625f, 0.0625f, 1.0f, 0.9375f), // West bar
+                new Cuboidf(0.9375f, 0.9375f, 0.0625f, 1.0f, 1.0f, 0.9375f)    // East bar
             };
         }
 
         public override Cuboidf[] GetSelectionBoxes(IBlockAccessor blockAccessor, BlockPos pos)
         {
-            return new Cuboidf[] { new Cuboidf(0.125f, 0.875f, 0.125f, 0.875f, 1.0f, 0.875f) };
+            return new Cuboidf[] { new Cuboidf(0.0f, 0.9375f, 0.0f, 1.0f, 1.0f, 1.0f) };
         }
     }
 }
